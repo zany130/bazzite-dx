e0b577c202fdf544580c0be2fe6c9e45f45edafe
Andres <andresdortiz@gmail.com>
2025-08-29 16:46:50 -0400

    enable terra repo

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,18 +9,21 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# Enable Terra Repository
+echo 'Enabling Terra Repository.'
+sudo sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/terra.repo
+ 
 # this installs a package from fedora repos
 dnf5 install -y \
 coolercontrol \
 kvantum \
 liqudctl \
-refind \
-refind-tools \
+rEFInd \
+rEFInd-tools \
 sbctl \
 solaar
 
 # Use a COPR Example:
-
 #dnf5 -y copr enable agundur/KCast
 #dnf5 -y install kcast
 
@@ -31,13 +34,13 @@ curl --retry 3 -Lo /usr/share/gamescope-session-plus/bootstrap_steam.tar.gz http
 curl --retry 3 -Lo /etc/sddm.conf.d/steamos.conf https://raw.githubusercontent.com/ublue-os/bazzite/refs/heads/main/system_files/deck/shared/etc/sddm.conf.d/steamos.conf && \
 curl --retry 3 -Lo /etc/sddm.conf.d/virtualkbd.conf https://raw.githubusercontent.com/ublue-os/bazzite/refs/heads/main/system_files/deck/shared/etc/sddm.conf.d/virtualkbd.conf
 
- sed -i -E \
-      -e 's/^(action\/switch_user)=true/\1=false/' \
-      -e 's/^(action\/start_new_session)=true/\1=false/' \
-      -e 's/^(action\/lock_screen)=true/\1=false/' \
-      -e 's/^(kcm_sddm\.desktop)=true/\1=false/' \
-      -e 's/^(kcm_plymouth\.desktop)=true/\1=false/' \
-      /etc/xdg/kdeglobals
+sed -i -E \
+     -e 's/^(action\/switch_user)=true/\1=false/' \
+     -e 's/^(action\/start_new_session)=true/\1=false/' \
+     -e 's/^(action\/lock_screen)=true/\1=false/' \
+     -e 's/^(kcm_sddm\.desktop)=true/\1=false/' \
+     -e 's/^(kcm_plymouth\.desktop)=true/\1=false/' \
+     /etc/xdg/kdeglobals
 
- systemctl enable bazzzite-autologin.service
- systemctl disable uupd.timer
+systemctl enable bazzzite-autologin.service
+systemctl disable uupd.timer
