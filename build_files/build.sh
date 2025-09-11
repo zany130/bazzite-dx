457e0fc5abbad5957dc07a1da92e72799f06aae4
Andres <andresdortiz@gmail.com>
2025-09-11 10:50:04 -0400

    fix builds hopefully

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,18 +13,11 @@ set -ouex pipefail
 echo 'Enabling Terra Repository.'
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/terra.repo
 
-# Use a COPR Example:
-#copr enable agundur/KCast /
-copr enable deltacopy/darkly
-copr enable matinlotfali/KDE-Rounded-Corners 
-
 # this installs a package from fedora repos
 dnf5 install -y \
 coolercontrol \
-darkly \
 google-authenticator \
 kvantum \
-kwin-effect-roundcorners \
 liquidctl \
 plasma-discover \
 plasma-discover-kns \
@@ -36,6 +29,14 @@ rEFInd-tools \
 sbctl \
 smb4k \
 solaar
+
+# Use a COPR Example:
+#copr enable agundur/KCast /
+copr enable deltacopy/darkly
+copr enable matinlotfali/KDE-Rounded-Corners 
+dnf5 install -y \
+darkly \
+kwin-effect-roundcorners
 
 #### Example for enabling a System Unit File
 
