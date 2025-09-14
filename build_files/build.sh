4cf81d5bd1a3218091c397023c22b33ff159df22
Andres <andresdortiz@gmail.com>
2025-09-14 19:29:16 -0400

    fix rpm-fusion repo for good hopefully

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,12 +15,8 @@ sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/terra.repo
 
 # Enable RPM Fusion Repository
 echo 'Enabling RPM Fusion Repository.'
-dnf5 install -y \
-https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-#sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-nonfree.repo
-#sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-free.repo
+sudo dnf5 config-manager setopt rpmfusion-nonfree.enabled=1
+sudo dnf5 config-manager setopt rpmfusion-free.enabled=1
 
 # this installs a package from fedora repos
 dnf5 install -y \
